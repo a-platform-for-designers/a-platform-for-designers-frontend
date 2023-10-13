0371e48ceef5d392b1892270722186a91fb44ff1
Anton Glazunov <kampel2012@yandex.ru>
2023-10-13 12:38:00 +0500

    feature: Добавить кнопку и её код

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import classes from "./App.module.sass";
 import Footer from "./components/footer/Footer";
+import "./utils/vkbutton.ts";
+import { redirectAuthHandler } from "./utils/vkbutton.ts";
 
 function App() {
   return (
     <div className={classes.app}>
+      <button onClick={redirectAuthHandler}>click me</button>
       Main <Footer />
     </div>
   );
--- src/utils/vkbutton.ts
+++ src/utils/vkbutton.ts
@@ -1,10 +1,5 @@
-import {
-  ButtonOneTapSkin,
-  Config,
-  Connect,
-  ConnectEvents,
-  VKAuthButtonCallbackResult,
-} from "@vkontakte/superappkit";
+import { Config, Connect } from "@vkontakte/superappkit";
+
 Config.init({
   appId: 51768929, // Тут нужно подставить ID своего приложения.
 
@@ -18,53 +13,23 @@ Config.init({
   },
 });
 
-const oneTapButton = Connect.buttonOneTapAuth({
-  callback: (event: VKAuthButtonCallbackResult) => {
-    const { type } = event;
+export const redirectAuthHandler = () =>
+  Connect.redirectAuth({
+    url: "http://localhost:5173/ok",
+    state: "",
+    source: "",
+    action: undefined,
+    screen: undefined,
+  });
 
-    if (!type) {
-      return;
-    }
+//TODO при переходе на страницу /ok - будет парсится строка с пейлоадом и передаваться на бек
+// ? http://localhost:5173/ok?payload=%7B%22type%22%3A%22silent_token%22%2C%22auth%22%3A1%2C%22user%22%3A%7B%22id%22%3A453651414%2C%22first_name%22%3A%22Антон
 
-    switch (type) {
-      case ConnectEvents.OneTapAuthEventsSDK.LOGIN_SUCCESS: // = 'VKSDKOneTapAuthLoginSuccess'
-        console.log(event);
-        return;
-      // Для этих событий нужно открыть полноценный VK ID чтобы
-      // пользователь дорегистрировался или подтвердил телефон
-      case ConnectEvents.OneTapAuthEventsSDK.FULL_AUTH_NEEDED: //  = 'VKSDKOneTapAuthFullAuthNeeded'
-      case ConnectEvents.OneTapAuthEventsSDK.PHONE_VALIDATION_NEEDED: // = 'VKSDKOneTapAuthPhoneValidationNeeded'
-      case ConnectEvents.ButtonOneTapAuthEventsSDK.SHOW_LOGIN: // = 'VKSDKButtonOneTapAuthShowLogin'
-        // url - строка с url, на который будет произведён редирект после авторизации.
-        // state - состояние вашего приложение или любая произвольная строка, которая будет добавлена к url после авторизации.
-        return Connect.redirectAuth({
-          url: "https://...",
-          state: "dj29fnsadjsd82...",
-        });
-      // Пользователь перешел по кнопке "Войти другим способом"
-      case ConnectEvents.ButtonOneTapAuthEventsSDK.SHOW_LOGIN_OPTIONS: // = 'VKSDKButtonOneTapAuthShowLoginOptions'
-        // Параметр screen: phone позволяет сразу открыть окно ввода телефона в VK ID
-        // Параметр url: ссылка для перехода после авторизации. Должен иметь https схему. Обязательный параметр.
-        return Connect.redirectAuth({ screen: "phone", url: "https://..." });
-    }
+/* // парс
+const encodedString = "ваша закодированная строка"; // вставьте вашу закодированную строку здесь
+const decodedString = decodeURIComponent(encodedString);
+const parsedObject = JSON.parse(decodedString);
 
-    return;
-  },
-  // Не обязательный параметр с настройками отображения OneTap
-  options: {
-    showAlternativeLogin: true,
-    showAgreements: true,
-    displayMode: "default",
-    langId: 0,
-    buttonSkin: "flat" as ButtonOneTapSkin,
-    buttonStyles: {
-      borderRadius: 8,
-      height: 50,
-    },
-  },
-});
-
-// Получить iframe можно с помощью метода getFrame()
-if (oneTapButton) {
-  document.body.appendChild(oneTapButton.getFrame()!);
-}
+// Теперь parsedObject содержит ваш распарсенный объект JSON
+console.log(parsedObject);
+*/
