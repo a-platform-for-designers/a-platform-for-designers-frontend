89924c4b5898a898d0ecbea02d76bc7c88b0a836
Anton Glazunov <kampel2012@yandex.ru>
2023-10-20 16:14:36 +0500

    Feature: добавить настройку роутинга, стора + типы

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router";
 import classes from "./App.module.scss";
-import Footer from "./components/footer/Footer";
+import MainPage from "./pages/MainPage/MainPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
     <div className={classes.app}>
-      Main <Footer />
+      <Routes>
+        <Route path="/" Component={MainPage} />
+        <Route
+          path="/profile"
+          element={<ProtectedRoute Component={ProfilePage} />}
+        />
+        <Route path="*" Component={ErrorPage} />
+      </Routes>
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import "./Header.sass";
 
-function Header() {
+const Header: React.FC = () => {
   return <header className="header">Header</header>;
-}
+};
 
 export default Header;
