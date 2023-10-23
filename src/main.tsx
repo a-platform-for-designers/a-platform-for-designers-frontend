7a8272092a43ef830002d68c7cd398cac06c80f0
Anton Glazunov <kampel2012@yandex.ru>
2023-10-23 15:34:54 +0500

    Feature: добавить google auth

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store.ts";
 import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="406355123322-vv7qk2u0mk3h3radslhug8v1h3c1bqrl.apps.googleusercontent.com">
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,81 @@
+import { useEffect, useState } from "react";
 import "./MainPage.scss";
+import axios from "axios";
+import {
+  TokenResponse,
+  googleLogout,
+  useGoogleLogin,
+} from "@react-oauth/google";
 
 const MainPage: React.FC = () => {
-  return <div>MainPage</div>;
+  type TGoogleUser = TokenResponse | null;
+  type TGoogleProfile = {
+    email: string;
+    family_name: string;
+    given_name: string;
+    id: string;
+    locale: string;
+    name: string;
+    picture: string;
+    verified_email: boolean;
+  } | null;
+
+  const [user, setUser] = useState<TGoogleUser>(null);
+  const [profile, setProfile] = useState<TGoogleProfile>(null);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      console.log("Успешно вошли", codeResponse);
+      setUser(codeResponse);
+    },
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get<TGoogleProfile>(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  return (
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        <button onClick={() => login()}>Sign in with Google 🚀 </button>
+      )}
+    </div>
+  );
 };
 
 export default MainPage;
