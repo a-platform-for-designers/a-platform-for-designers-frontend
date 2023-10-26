a9784bdca93fa454ab74bf7116fdeecfda731bf7
Anton Glazunov <kampel2012@yandex.ru>
2023-10-26 12:53:08 +0500

    Feature: добавить второй вариант single

--- src/components/UI/MyDropDown/MyDropDown.tsx
+++ src/components/UI/MyDropDown/MyDropDown.tsx
@@ -6,52 +6,107 @@ import "./MyDropDown.scss";
 import { StyledEngineProvider } from "@mui/material/styles";
 import Chip from "@mui/material/Chip";
 import closeIcon from "../../../assets/icons/close.svg";
+import { useState } from "react";
 
-const MyDropDown: React.FC = () => {
+interface IMyDropDownProps {
+  variant?: "multiple" | "single";
+  options: string[];
+}
+
+const MyDropDown: React.FC<IMyDropDownProps> = ({
+  variant = "single",
+  options,
+}) => {
   const checkedIcon = <CheckOutlinedIcon fontSize="small" color="action" />;
+  const [valueSingle, setValueSingle] = useState<string | null>(null);
+  const [valueMulty, setValueMulty] = useState<string[]>([]);
 
-  return (
-    <StyledEngineProvider injectFirst>
-      <Autocomplete
-        multiple
-        className="myDropDown"
-        options={top100Films}
-        disableCloseOnSelect
-        getOptionLabel={(option) => option}
-        renderTags={(value: string[], getTagProps) =>
-          value.map((option: string, index: number) => (
-            <Chip
-              variant="filled"
-              label={option}
-              {...getTagProps({ index })}
-              deleteIcon={<img src={closeIcon} />}
-            />
-          ))
-        }
-        renderOption={(props, option, { selected }) => (
-          <li {...props} className="myDropDown__element">
-            {option}
-            <Checkbox
-              icon={<></>}
-              checkedIcon={checkedIcon}
-              className="myDropDown__checkbox"
-              checked={selected}
-            />
-          </li>
-        )}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="filled"
-            placeholder="Favorites"
-            className="myDropDown__input"
+  switch (variant) {
+    case "multiple":
+      return (
+        <StyledEngineProvider injectFirst>
+          <Autocomplete
+            value={valueMulty}
+            onChange={(_: unknown, newValue: string[]) => {
+              setValueMulty(newValue);
+            }}
+            multiple
+            className="myDropDown"
+            options={options}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option}
+            renderTags={(value: string[], getTagProps) =>
+              value.map((option: string, index: number) => (
+                <Chip
+                  variant="filled"
+                  label={option}
+                  {...getTagProps({ index })}
+                  deleteIcon={<img src={closeIcon} />}
+                />
+              ))
+            }
+            renderOption={(props, option, { selected }) => (
+              <li {...props} className="myDropDown__element">
+                {option}
+                <Checkbox
+                  icon={<></>}
+                  checkedIcon={checkedIcon}
+                  className="myDropDown__checkbox"
+                  checked={selected}
+                />
+              </li>
+            )}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="filled"
+                placeholder="Favorites"
+                className="myDropDown__input"
+              />
+            )}
           />
-        )}
-      />
-    </StyledEngineProvider>
-  );
+        </StyledEngineProvider>
+      );
+
+    case "single":
+      return (
+        <StyledEngineProvider injectFirst>
+          <Autocomplete
+            value={valueSingle}
+            onChange={(_: unknown, newValue: string | null) => {
+              setValueSingle(newValue);
+            }}
+            className="myDropDown"
+            options={options}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option}
+            renderOption={(props, option, { selected }) => (
+              <li {...props} className="myDropDown__element">
+                {option}
+                <Checkbox
+                  icon={<></>}
+                  checkedIcon={checkedIcon}
+                  className="myDropDown__checkbox"
+                  checked={selected}
+                />
+              </li>
+            )}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="filled"
+                placeholder="Favorites"
+                className="myDropDown__input"
+              />
+            )}
+          />
+        </StyledEngineProvider>
+      );
+      break;
+
+    default:
+      break;
+  }
 };
 
 export default MyDropDown;
-
-const top100Films = ["Анимация", "Загрузка", "Портфолио"];
