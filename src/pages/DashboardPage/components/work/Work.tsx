027ad82eabbeada83bac09ae4158f371a9d83e30
Байбаков Кирилл Дмитриевич <bkd@GosSort.local>
2023-11-29 17:48:37 +0300

    fix: resume page container added

--- src/pages/DashboardPage/components/work/Work.tsx
+++ src/pages/DashboardPage/components/work/Work.tsx
@@ -43,78 +43,86 @@ const Work: React.FC = () => {
   }
 
   return (
-    <div className={classes.work__sections}>
-      <Box className={classes.work__section}>
-        <Typography className={classes.work__section_title}>Статус</Typography>
-        <div className={classes.work__section_wrapper}>
-          <FormControl>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="searching"
-              name="radio-buttons-group"
-            >
-              <FormControlLabel
-                value="searching"
-                control={<Radio />}
-                label="Ищу заказы"
-              />
-              <FormControlLabel
-                value="not-searching"
-                control={<Radio />}
-                label="Не ищу заказы"
-              />
-            </RadioGroup>
-          </FormControl>
-        </div>
-      </Box>
-      <Box className={classes.work__section}>
-        <Typography className={classes.work__section_title}>
-          Инструменты
-        </Typography>
-        <div className={classes.work__section_wrapper}>
-          <MyMultipleDropDown
-            className={classes.work__myDrowDown}
-            value={tools}
-            options={LISTS.LIST_TOOLS}
-            onChange={handleSetTools}
-            placeholder="Какие программы используете"
-          />
-        </div>
-      </Box>
-      <Box className={classes.work__section}>
-        <Typography className={classes.work__section_title}>Навыки</Typography>
-        <div className={classes.work__section_wrapper}>
-          <MyMultipleDropDown
-            className={classes.work__myDrowDown}
-            value={skills}
-            options={LISTS.LIST_SKILLS}
-            onChange={handleSetSkills}
-            placeholder="Выберите навыки"
-          />
-        </div>
-      </Box>
-      <Box className={classes.work__section}>
-        <Typography className={classes.work__section_title}>О себе</Typography>
-        <div className={classes.work__section_wrapper}>
-          <MyInput
-            data={about}
-            variant="textarea-label-without"
-            placeholder="Расскажите о себе, опыте и о том, что считаете важным..."
-            minRows={10}
-            maxLength={500}
-            className={classes.work__section_textarea}
-          />
-        </div>
-      </Box>
-      <Box textAlign={"center"}>
-        <MyButton
-          className={classes.work__btn}
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Сохранить
-        </MyButton>
-      </Box>
+    <div className={classes.work}>
+      <div className={classes.work__sections}>
+        <Box className={classes.work__section}>
+          <Typography className={classes.work__section_title}>
+            Статус
+          </Typography>
+          <div className={classes.work__section_wrapper}>
+            <FormControl>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="searching"
+                name="radio-buttons-group"
+              >
+                <FormControlLabel
+                  value="searching"
+                  control={<Radio />}
+                  label="Ищу заказы"
+                />
+                <FormControlLabel
+                  value="not-searching"
+                  control={<Radio />}
+                  label="Не ищу заказы"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+        </Box>
+        <Box className={classes.work__section}>
+          <Typography className={classes.work__section_title}>
+            Инструменты
+          </Typography>
+          <div className={classes.work__section_wrapper}>
+            <MyMultipleDropDown
+              className={classes.work__myDrowDown}
+              value={tools}
+              options={LISTS.LIST_TOOLS}
+              onChange={handleSetTools}
+              placeholder="Какие программы используете"
+            />
+          </div>
+        </Box>
+        <Box className={classes.work__section}>
+          <Typography className={classes.work__section_title}>
+            Навыки
+          </Typography>
+          <div className={classes.work__section_wrapper}>
+            <MyMultipleDropDown
+              className={classes.work__myDrowDown}
+              value={skills}
+              options={LISTS.LIST_SKILLS}
+              onChange={handleSetSkills}
+              placeholder="Выберите навыки"
+            />
+          </div>
+        </Box>
+        <Box className={classes.work__section}>
+          <Typography className={classes.work__section_title}>
+            О себе
+          </Typography>
+          <div className={classes.work__section_wrapper}>
+            <MyInput
+              data={about}
+              variant="textarea-label-without"
+              placeholder="Расскажите о себе, опыте и о том, что считаете важным..."
+              minRows={10}
+              maxLength={500}
+              className={classes.work__section_textarea}
+            />
+          </div>
+        </Box>
+        <Box textAlign={"center"}>
+          <MyButton
+            className={classes.work__btn}
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Сохранить
+          </MyButton>
+        </Box>
+      </div>
     </div>
   );
 };
