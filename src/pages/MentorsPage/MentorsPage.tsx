f7bb59bcf3c4803ca4313a9ccaaa76cb4440481e
Anastasiia <nastya.kll@yandex.ru>
2023-12-11 19:42:09 +0300

    fix: исправить отображение менторов

--- src/pages/MentorsPage/MentorsPage.tsx
+++ src/pages/MentorsPage/MentorsPage.tsx
@@ -28,24 +28,6 @@ const MentorsPage: React.FC = () => {
     fetchData();
   }, []);
 
-  // function getMentors() {
-  //   users.map((user) => {
-  //     const name: string = "name";
-  //     const hasMentor =
-  //       Array.isArray(user.specialization) &&
-  //       user.specialization.some((obj: object) => String(obj[name as keyof typeof obj]) === "Ментор");
-  //     if (hasMentor) {
-  //       console.log("В массиве есть объект с name 'Ментор'");
-  //     } else {
-  //       console.log(
-  //         "В массиве нет объекта с name 'Ментор' или массив пуст или равен null"
-  //       );
-  //     }
-  //   });
-  // }
-
-  // getMentors();
-
   return (
     <StyledEngineProvider injectFirst>
       <Box component="main" className="designersPage">
