25a77fdf72b6a4eb9a3696c96036d24dbf0b3fd9
Anastasiia <nastya.kll@yandex.ru>
2023-12-11 13:33:41 +0300

    feature: отобразить ошибку с сервера (частично)

--- src/api/services/authService.ts
+++ src/api/services/authService.ts
@@ -3,8 +3,13 @@ import api from "../api";
 
 const authService = {
   login: async (data: IAuthUserRequest): Promise<IToken> => {
-    const response = await api.post<IToken>("/auth/token/login", data);
-    return response.data;
+    try {
+      const response = await api.post<IToken>("/auth/token/login", data);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   },
 
   logout: async (): Promise<void> => {
