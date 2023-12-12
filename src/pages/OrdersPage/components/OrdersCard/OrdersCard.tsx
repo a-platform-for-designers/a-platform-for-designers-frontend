9a44b3a6c1df2cc457ec273a0538c6d72ab5401b
Anna Haratova <black-isida@yandex.ru>
2023-12-12 15:23:52 +0300

    редактирование карточки заказа

--- src/pages/OrdersPage/components/OrdersCard/OrdersCard.tsx
+++ src/pages/OrdersPage/components/OrdersCard/OrdersCard.tsx
@@ -3,7 +3,6 @@ import "./OrdersCard.scss";
 import MyButton from "@/shared/UI/MyButton/MyButton";
 import { IOrdersList, IUserInfo } from "@/types";
 import { useEffect, useState } from "react";
-
 import FavouritesIcon from "../../../../assets/icons/FavouritesDark.svg";
 import FavouritesIconActive from "../../../../assets/icons/FavouritesActive.svg";
 
@@ -17,7 +16,8 @@ const OrdersCard: React.FC<IProps> = ({ order, openPopup }) => {
   const [isFavourite, setIsFavourite] = useState<boolean>(false);
   const [customerSpecialization, setCustomerSpecialization] =
     useState<string>("");
-
+  const [isCurrentUser, setIsCurrentUser] = useState<boolean>(false); // при значении true карточка выглядит как будто она создана самим заказчиком. False - так видят все остальные
+  const countResponse = 10; //количество откликов
   useEffect(() => {
     if (order.specialization.name === "Графический дизайн") {
       setCustomerSpecialization("Графический дизайнер");
@@ -62,69 +62,99 @@ const OrdersCard: React.FC<IProps> = ({ order, openPopup }) => {
     setIsFavourite(true);
   }
 
+  function handleEditCard() {
+    setIsCurrentUser(false);
+  }
+
   return (
     <Box className="ordersCard">
-      <div className="ordersCard__header">
-        <div className="ordersCard__user">
-          <Avatar className="ordersCard__avatar" src={order.customer.photo} />
-          <Typography component="h2" className="ordersCard__name">
-            {userInfo.name}
+      <div>
+        <div className="ordersCard__header">
+          <div className="ordersCard__user">
+            <Avatar className="ordersCard__avatar" src={order.customer.photo} />
+            <Typography component="h2" className="ordersCard__name">
+              {userInfo.name}
+            </Typography>
+          </div>
+          {!isCurrentUser ? (
+            <>
+              <IconButton aria-label="favourite" onClick={handleFavourite}>
+                {!isFavourite ? (
+                  <img
+                    className="ordersCard__favourite-icon"
+                    src={FavouritesIcon}
+                    alt="Иконка избранное"
+                  />
+                ) : (
+                  <img
+                    className="ordersCard__favourite-icon"
+                    src={FavouritesIconActive}
+                    alt="Иконка избранное"
+                  />
+                )}
+              </IconButton>
+            </>
+          ) : (
+            <>
+              <div className="ordersCard__counts">{countResponse}</div>
+              <IconButton aria-label="favourite" onClick={handleFavourite}>
+                <img
+                  className="ordersCard__favourite-icon"
+                  src={FavouritesIconActive}
+                  alt="Иконка редактирования"
+                />
+              </IconButton>
+            </>
+          )}
+        </div>
+
+        <div>
+          <Typography component="h3" className="ordersCard__title">
+            {order.title && order.title}
+          </Typography>
+          <Typography component="p" className="ordersCard__description">
+            {order.description && order.description}
+          </Typography>
+          <Typography component="p" className="ordersCard__specialization">
+            Кто нужен: {customerSpecialization}
+          </Typography>
+          <Typography component="p" className="ordersCard__specialization">
+            Сфера: {order.sphere && order.sphere.name}
+          </Typography>
+          <Typography component="p" className="ordersCard__price">
+            {order.payment && order.payment} ₽
           </Typography>
         </div>
-        <IconButton aria-label="favourite" onClick={handleFavourite}>
-          {!isFavourite ? (
-            <img
-              className="ordersCard__favourite-icon"
-              src={FavouritesIcon}
-              alt="Иконка меню"
-            />
-          ) : (
-            <img
-              className="ordersCard__favourite-icon"
-              src={FavouritesIconActive}
-              alt="Иконка меню"
-            />
-          )}
-        </IconButton>
       </div>
-
-      <div>
-        <Typography component="h3" className="ordersCard__title">
-          {order.title && order.title}
-        </Typography>
-        <Typography component="p" className="ordersCard__title">
-          {order.description && order.description}
-        </Typography>
-        <Typography component="p" className="ordersCard__specialization">
-          Кто нужен: {customerSpecialization}
-        </Typography>
-        <Typography component="p" className="ordersCard__specialization">
-          Сфера: {order.sphere && order.sphere.name}
-        </Typography>
-        <Typography component="p" className="ordersCard__price">
-          {order.payment && order.payment} ₽
-        </Typography>
-      </div>
-
-      <div className="ordersCard__buttons">
-        <MyButton
-          type="button"
-          variant="outlined"
-          size="large"
-          onClick={handlePopupOpen}
-        >
-          Написать
-        </MyButton>
-        <MyButton
-          type="button"
-          variant="outlined"
-          size="large"
-          onClick={handleReply}
-          className="ordersCard__button"
-        >
-          {!reply ? "Откликнуться" : "Удалить отклик"}
-        </MyButton>
-      </div>
+      {!isCurrentUser ? (
+        <>
+          <div className="ordersCard__buttons">
+            <MyButton
+              type="button"
+              variant="outlined"
+              size="large"
+              onClick={handlePopupOpen}
+            >
+              Написать
+            </MyButton>
+            <MyButton
+              type="button"
+              variant="outlined"
+              size="large"
+              onClick={handleReply}
+              className="ordersCard__button"
+            >
+              {!reply ? "Откликнуться" : "Удалить отклик"}
+            </MyButton>
+          </div>
+        </>
+      ) : (
+        <div className="ordersCard__buttons">
+          <MyButton type="button" size="large" onClick={handleEditCard}>
+            Написать
+          </MyButton>
+        </div>
+      )}
     </Box>
   );
 };
