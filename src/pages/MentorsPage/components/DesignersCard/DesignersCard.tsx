1fd1e40ebbc052c276c79d54ee0121c44d2bcf6d
Anastasiia <nastya.kll@yandex.ru>
2023-12-12 17:29:46 +0300

    fix: bugs fixed

--- src/pages/MentorsPage/components/DesignersCard/DesignersCard.tsx
+++ src/pages/MentorsPage/components/DesignersCard/DesignersCard.tsx
@@ -1,9 +1,12 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Typography, StyledEngineProvider } from "@mui/material";
 import "./DesignersCard.scss";
 import MyButton from "@/shared/UI/MyButton/MyButton";
 import MySwiper from "@/shared/UI/MySwiper/MySwiper";
 import { IUserWithLastCases } from "@/types";
 import { useNavigate } from "react-router-dom";
+import { MessagePopup } from "@/pages/OrdersPage/components";
+import { useAppSelector } from "@/hooks/reduxHooks";
+import { useState } from "react";
 
 interface IProps {
   user: IUserWithLastCases;
@@ -11,6 +14,8 @@ interface IProps {
 
 const DesignersCard: React.FC<IProps> = ({ user }) => {
   const navigate = useNavigate();
+  const { isAuth } = useAppSelector((state) => state.auth);
+  const [openPopup, setOpenPopup] = useState<boolean>(false);
 
   function setSpecializations(): string[] {
     const name: string = "name";
@@ -23,40 +28,64 @@ const DesignersCard: React.FC<IProps> = ({ user }) => {
     return arr;
   }
 
+  function handleClick() {
+    if (isAuth) {
+      setOpenPopup(true);
+    }
+  }
+
+  function handlePopupClose() {
+    setOpenPopup(false);
+  }
+
   return (
-    <Box className="designersCard">
-      <div className="designersCard__info">
-        <div
-          className="designersCard__infoData"
-          onClick={() => {
-            navigate(`/profile/${user.id}/`);
-          }}
-        >
-          <Avatar
-            className="designersCard__avatar"
-            src={user.photo}
-            sx={{ backgroundColor: "#4F378B", color: "#EADDFF" }}
-          >{`${user?.first_name[0]}${user?.last_name[0]}`}</Avatar>
-          <Typography component="h2" className="designersCard__name">
-            {user.first_name} {user.last_name}
-          </Typography>
-          <Typography paragraph className="designersCard__job">
-            {setSpecializations() && setSpecializations().join(", ")}
-          </Typography>
-          <Typography paragraph className="designersCard__country">
-            {user.profiledesigner?.country}
-          </Typography>
+    <StyledEngineProvider injectFirst>
+      <Box className="designersCard">
+        <div className="designersCard__info">
+          <div
+            className="designersCard__infoData"
+            onClick={() => {
+              navigate(`/profile/${user.id}/`);
+            }}
+          >
+            <Avatar
+              className="designersCard__avatar"
+              src={user.photo}
+              sx={{ backgroundColor: "#4F378B", color: "#EADDFF" }}
+            >{`${user?.first_name[0]}${user?.last_name[0]}`}</Avatar>
+            <Typography component="h2" className="designersCard__name">
+              {user.first_name} {user.last_name}
+            </Typography>
+            <Typography paragraph className="designersCard__job">
+              {setSpecializations() && setSpecializations().join(", ")}
+            </Typography>
+            <Typography paragraph className="designersCard__country">
+              {user.profiledesigner?.country}
+            </Typography>
+          </div>
+          <div>
+            <MyButton
+              variant="outlined"
+              onClick={() => {
+                handleClick();
+              }}
+            >
+              Написать
+            </MyButton>
+          </div>
         </div>
-        <div>
-          <MyButton variant="outlined" onClick={() => {}}>
-            Написать
-          </MyButton>
-        </div>
-      </div>
-      {user.last_cases.map((item) => (
-        <MySwiper key={item.id} item={item} />
-      ))}
-    </Box>
+        {user.last_cases.map((item) => (
+          <MySwiper
+            key={item.id}
+            item={item}
+            onClick={() => navigate(`/case/${item.id}`)}
+          />
+        ))}
+      </Box>
+      {openPopup ? (
+        <MessagePopup open={openPopup} onClose={handlePopupClose} />
+      ) : null}
+    </StyledEngineProvider>
   );
 };
 
