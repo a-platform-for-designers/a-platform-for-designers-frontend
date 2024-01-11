25596132192d2c8cdbcd4aee5b7d1089f13c47dc
Анна Харатова <black-isida@yandex.ru>
2024-01-11 18:55:16 +0300

    fix(OrderPage):исправлена ошибка типизации

--- src/api/services/ordersService.ts
+++ src/api/services/ordersService.ts
@@ -1,4 +1,8 @@
-import { IOrdersResponse, IOrderResponse } from "../../types";
+import {
+  IOrdersResponse,
+  IOrderResponse,
+  IOrderInfoResponse,
+} from "../../types";
 import api from "../api";
 
 const ordersService = {
@@ -20,8 +24,8 @@ const ordersService = {
     return response.data;
   },
 
-  getOrderInfo: async (id: number): Promise<IOrdersResponse> => {
-    const response = await api.get<IOrdersResponse>(`/orders/${id}/`);
+  getOrderInfo: async (id: number): Promise<IOrderInfoResponse> => {
+    const response = await api.get<IOrderInfoResponse>(`/orders/${id}/`);
     return response.data;
   },
 
