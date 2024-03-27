52821c58c9f196a4491598848c7b24d79b29452b
gineff <canone@inbox.ru>
2024-03-27 16:19:28 +0300

    add activate account page

--- src/pages/ActivateAccount/ActivateAccount.tsx
+++ src/pages/ActivateAccount/ActivateAccount.tsx
@@ -1,13 +1,48 @@
 import { MainPage } from "..";
-import ActivateAccount from "./components/ActivateAccount/ActivateAccount";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from "@/hooks/reduxHooks";
+import { enqueueSnackbar } from "notistack";
+import {
+  activateAccount,
+  resetAuthErrors,
+  setCurrentScreen,
+} from "@/redux/slices/authSlice";
+import { Screens } from "@/types";
 
 const ActivateAccountPage = () => {
-  return (
-    <>
-      <MainPage />
-      <ActivateAccount />
-    </>
-  );
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { errorMessages } = useAppSelector((state) => state.auth);
+  const { uid = "", token = "" } = useParams();
+
+  const handleActivate = async () => {
+    const response = await dispatch(activateAccount({ uid, token }));
+    if (!("error" in response)) {
+      navigate("/");
+      dispatch(setCurrentScreen({ screen: Screens.SignIn }));
+    } else {
+      dispatch(setCurrentScreen({ screen: Screens.Activation }));
+    }
+  };
+
+  useEffect(() => {
+    handleActivate();
+  }, [uid, token, dispatch]);
+
+  useEffect(() => {
+    errorMessages.forEach((message) => {
+      enqueueSnackbar({
+        variant: "error",
+        message,
+      });
+    });
+    return () => {
+      dispatch(resetAuthErrors());
+    };
+  }, [errorMessages, dispatch]);
+
+  return <MainPage />;
 };
 
 export default ActivateAccountPage;
